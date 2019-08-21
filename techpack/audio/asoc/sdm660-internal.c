f7f7f8853c6a30f7d2ba818d6e93f125330ea905
1582130940 <1582130940@qq.com>
2019-08-21 18:37:33 +0800

    sound/soc: Import Xiaomi changes

--- techpack/audio/asoc/sdm660-internal.c
+++ techpack/audio/asoc/sdm660-internal.c
@@ -1203,7 +1203,11 @@ static void *def_msm_int_wcd_mbhc_cal(void)
 		return NULL;
 
 #define S(X, Y) ((WCD_MBHC_CAL_PLUG_TYPE_PTR(msm_int_wcd_cal)->X) = (Y))
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	S(v_hs_max, 1600);
+#else
 	S(v_hs_max, 1500);
+#endif
 #undef S
 #define S(X, Y) ((WCD_MBHC_CAL_BTN_DET_PTR(msm_int_wcd_cal)->X) = (Y))
 	S(num_btn, WCD_MBHC_DEF_BUTTONS);
@@ -1226,6 +1230,18 @@ static void *def_msm_int_wcd_mbhc_cal(void)
 	 * 210-290 == Button 2
 	 * 360-680 == Button 3
 	 */
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	btn_low[0] = 75;
+	btn_high[0] = 75;
+	btn_low[1] = 246;
+	btn_high[1] = 246;
+	btn_low[2] = 440;
+	btn_high[2] = 440;
+	btn_low[3] = 440;
+	btn_high[3] = 440;
+	btn_low[4] = 440;
+	btn_high[4] = 440;
+#else
 	btn_low[0] = 75;
 	btn_high[0] = 75;
 	btn_low[1] = 150;
@@ -1236,6 +1252,7 @@ static void *def_msm_int_wcd_mbhc_cal(void)
 	btn_high[3] = 450;
 	btn_low[4] = 500;
 	btn_high[4] = 500;
+#endif
 
 	return msm_int_wcd_cal;
 }
@@ -2632,6 +2649,9 @@ static struct snd_soc_dai_link msm_mi2s_be_dai_links[] = {
 #ifdef CONFIG_SND_SOC_TAS2557
 		.codec_name = "tas2557.6-004c",
 		.codec_dai_name = "tas2557 ASI1",
+#elif CONFIG_MACH_XIAOMI_CLOVER
+		.codec_name     = "tas2557s.6-004c",
+		.codec_dai_name = "tas2557 Stereo ASI1",
 #else
 		.codec_name = "msm-stub-codec.1",
 		.codec_dai_name = "msm-stub-rx",
@@ -2652,6 +2672,9 @@ static struct snd_soc_dai_link msm_mi2s_be_dai_links[] = {
 #ifdef CONFIG_SND_SOC_TAS2557
 		.codec_name = "tas2557.6-004c",
 		.codec_dai_name = "tas2557 ASI1",
+#elif CONFIG_MACH_XIAOMI_CLOVER
+		.codec_name     = "tas2557s.6-004c",
+		.codec_dai_name = "tas2557 Stereo ASI1",
 #else
 		.codec_name = "msm-stub-codec.1",
 		.codec_dai_name = "msm-stub-tx",
