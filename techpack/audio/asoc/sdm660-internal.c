a6565a36745cdd77bf9b1f6d2757639cfed911a2
1582130940 <1582130940@qq.com>
2019-08-21 18:37:33 +0800

    sound/soc: Import Xiaomi changes
    Signed-off-by: sabar <sabar.pakpahan@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- techpack/audio/asoc/sdm660-internal.c
+++ techpack/audio/asoc/sdm660-internal.c
@@ -1248,7 +1248,12 @@ static void *def_msm_int_wcd_mbhc_cal(void)
 		return NULL;
 
 #define S(X, Y) ((WCD_MBHC_CAL_PLUG_TYPE_PTR(msm_int_wcd_cal)->X) = (Y))
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	S(v_hs_max, 1600);
+#else
 	S(v_hs_max, 1700);
+#endif
+
 #undef S
 #define S(X, Y) ((WCD_MBHC_CAL_BTN_DET_PTR(msm_int_wcd_cal)->X) = (Y))
 	S(num_btn, WCD_MBHC_DEF_BUTTONS);
@@ -1271,6 +1276,18 @@ static void *def_msm_int_wcd_mbhc_cal(void)
 	 * 210-290 == Button 2
 	 * 360-680 == Button 3
 	 */
+#elif defined(CONFIG_MACH_XIAOMI_CLOVER)
+	btn_low[0] = 75;
+	btn_high[0] = 75;
+	btn_low[1] = 246;
+	btn_high[1] = 246;
+	btn_low[2] = 440;
+	btn_high[2] = 440;
+	btn_low[3] = 440;
+	btn_high[3] = 440;
+	btn_low[4] = 440;
+	btn_high[4] = 440;
+#else
 	btn_low[0] = 75;
 	btn_high[0] = 75;
 	btn_low[1] = 225;
@@ -2675,8 +2692,13 @@ static struct snd_soc_dai_link msm_mi2s_be_dai_links[] = {
 		.stream_name = "Primary MI2S Playback",
 		.cpu_dai_name = "msm-dai-q6-mi2s.0",
 		.platform_name = "msm-pcm-routing",
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+		.codec_name     = "tas2557s.6-004c",
+		.codec_dai_name = "tas2557 Stereo ASI1",
+#else
 		.codec_name = "msm-stub-codec.1",
 		.codec_dai_name = "msm-stub-rx",
+#endif
 		.no_pcm = 1,
 		.dpcm_playback = 1,
 		.id = MSM_BACKEND_DAI_PRI_MI2S_RX,
@@ -2690,8 +2712,13 @@ static struct snd_soc_dai_link msm_mi2s_be_dai_links[] = {
 		.stream_name = "Primary MI2S Capture",
 		.cpu_dai_name = "msm-dai-q6-mi2s.0",
 		.platform_name = "msm-pcm-routing",
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+		.codec_name     = "tas2557s.6-004c",
+		.codec_dai_name = "tas2557 Stereo ASI1",
+#else
 		.codec_name = "msm-stub-codec.1",
 		.codec_dai_name = "msm-stub-tx",
+#endif
 		.no_pcm = 1,
 		.dpcm_capture = 1,
 		.id = MSM_BACKEND_DAI_PRI_MI2S_TX,
