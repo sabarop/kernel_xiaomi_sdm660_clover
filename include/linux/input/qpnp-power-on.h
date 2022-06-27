ubports-clover/android_kernel_xiaomi_clover

include/linux: Import Xiaomi changes

include/linux/input/qpnp-power-on.h
@@ -59,6 +59,11 @@ enum pon_restart_reason {
 	PON_RESTART_REASON_DMVERITY_CORRUPTED	= 0x04,
 	PON_RESTART_REASON_DMVERITY_ENFORCE	= 0x05,
 	PON_RESTART_REASON_KEYS_CLEAR		= 0x06,
+#ifdef CONFIG_XIAOMI_CLOVER
+	PON_RESTART_REASON_NORMAL		= 0x20,
+	PON_RESTART_REASON_PANIC		= 0x21,
+	PON_RESTART_REASON_OTHER		= 0x00,
+#endif
 
 	/* 32 ~ 63 for OEMs/ODMs secific features */
 	PON_RESTART_REASON_OEM_MIN		= 0x20,
@@ -72,6 +77,11 @@ int qpnp_pon_trigger_config(enum pon_trigger_source pon_src, bool enable);
 int qpnp_pon_wd_config(bool enable);
 int qpnp_pon_set_restart_reason(enum pon_restart_reason reason);
 bool qpnp_pon_check_hard_reset_stored(void);
+
+#ifdef CONFIG_XIAOMI_CLOVER
+int qpnp_pon_is_lpk(void);
+int qpnp_pon_is_ps_hold_reset(void);
+#endif
 
 #else
 static int qpnp_pon_system_pwr_off(enum pon_power_off_type type)
@@ -96,6 +106,10 @@ static inline bool qpnp_pon_check_hard_reset_stored(void)
 {
 	return false;
 }
+#ifdef CONFIG_XIAOMI_CLOVER
+static inline int qpnp_pon_is_lpk(void) { return -ENODEV; }
+static inline int qpnp_pon_is_ps_hold_reset(void) { return -ENODEV; }
+#endif
 #endif
 
 #endif

include/linux/platform_data/lp855x.h
@@ -146,4 +146,9 @@ struct lp855x_platform_data {
 	struct lp855x_rom_data *rom_data;
 };
 
+#ifdef CONFIG_XIAOMI_CLOVER
+int lp855x_init(void);
+int lp855x_brightness_ctrl(u32 level);
+#endif
+
 #endif

sabarop/kernel_xiaomi_sdm660_clover

(cherry picked from commit 8ef01c67ab7fb87b81adba0f641306ccf64cecab)
Signed-off-by: pix106 <sbordenave@gmail.com>

include/linux/input/qpnp-power-on.h
@@ -56,6 +56,11 @@ enum pon_restart_reason {
 #endif
 	PON_RESTART_REASON_DMVERITY_ENFORCE	= 0x05,
 	PON_RESTART_REASON_KEYS_CLEAR		= 0x06,
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	PON_RESTART_REASON_NORMAL		= 0x20,
+	PON_RESTART_REASON_PANIC		= 0x21,
+	PON_RESTART_REASON_OTHER		= 0x00,
+#endif
 #ifdef CONFIG_MACH_LONGCHEER
 	PON_RESTART_REASON_PANIC		= 0x21,
 	PON_RESTART_REASON_NORMAL		= 0x22,
@@ -74,6 +79,11 @@ int qpnp_pon_modem_pwr_off(enum pon_power_off_type type);
 int qpnp_pon_is_lpk(void);
 #endif
 int qpnp_pon_is_ps_hold_reset(void);
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+int qpnp_pon_is_lpk(void);
+int qpnp_pon_is_ps_hold_reset(void);
+#endif
 
 #else
 
@@ -122,6 +132,10 @@ static inline int qpnp_pon_is_ps_hold_reset(void)
 {
 	return -ENODEV;
 }
+#endif
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+static inline int qpnp_pon_is_lpk(void) { return -ENODEV; }
+static inline int qpnp_pon_is_ps_hold_reset(void) { return -ENODEV; }
 #endif
 #endif
 

include/linux/platform_data/lp855x.h
@@ -146,4 +146,9 @@ struct lp855x_platform_data {
 	struct lp855x_rom_data *rom_data;
 };
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+int lp855x_init(void);
+int lp855x_brightness_ctrl(u32 level);
+#endif
+
 #endif