ubports-clover/android_kernel_xiaomi_clover

drivers/soc/qcom: Import Xiaomi changes

drivers/soc/qcom/subsystem_restart.c
@@ -1645,6 +1645,9 @@ struct subsys_device *subsys_register(struct subsys_desc *desc)
 	subsys->dev.release = subsys_device_release;
 	subsys->notif_state = -1;
 	subsys->desc->sysmon_pid = -1;
+#ifdef CONFIG_XIAOMI_CLOVER
+	subsys->restart_level = 1;
+#endif
 	strlcpy(subsys->desc->fw_name, desc->name,
 			sizeof(subsys->desc->fw_name));
 

sabarop/kernel_xiaomi_sdm660_clover

drivers/soc/qcom/subsystem_restart.c
@@ -1812,6 +1812,9 @@ struct subsys_device *subsys_register(struct subsys_desc *desc)
 	subsys->notif_state = -1;
 	subsys->desc->sysmon_pid = -1;
 	subsys->desc->state = NULL;
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	subsys->restart_level = 1;
+#endif
 	strlcpy(subsys->desc->fw_name, desc->name,
 			sizeof(subsys->desc->fw_name));
 