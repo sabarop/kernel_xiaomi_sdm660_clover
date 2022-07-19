1582130940/android_kernel_xiaomi_clover

drivers/video: Import Xiaomi changes

drivers/video/backlight/lp855x_bl.c
@@ -16,16 +16,38 @@
 #include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/of.h>
+#ifdef CONFIG_XIAOMI_CLOVER
+#include <linux/of_gpio.h>
+#endif
 #include <linux/platform_data/lp855x.h>
 #include <linux/pwm.h>
 #include <linux/regulator/consumer.h>
+
+#ifdef CONFIG_XIAOMI_CLOVER
+/* LP8556 */
+#define LP8556_DEVICE_ID		0x05
+
+/* LP8556 Registers */
+#define LP8556_BRIGHTNESS_CTRL		0x00
+#define LP8556_DEVICE_CTRL		0x01
+#define LP8556_STATUS_REG		0x02
+#define LP8556_ID_REG			0x03
+#define LP8556_DIRECT_CTRL		0x04
+#define LP8556_REG_BRIGHTNESS_LSB   0x10
+#define LP8556_REG_BRIGHTNESS_MSB   0x11
+#define LP8556_STRING_ENABLE	0x16
+#define LP8556_EPROM_START		0x00
+#define LP8556_EPROM_CFG2		0xA2
+#endif
 
 /* LP8550/1/2/3/6 Registers */
 #define LP855X_BRIGHTNESS_CTRL		0x00
 #define LP855X_DEVICE_CTRL		0x01
 #define LP855X_EEPROM_START		0xA0
 #define LP855X_EEPROM_END		0xA7
+#ifndef CONFIG_XIAOMI_CLOVER
 #define LP8556_EPROM_START		0xA0
+#endif
 #define LP8556_EPROM_END		0xAF
 
 /* LP8555/7 Registers */
@@ -78,10 +100,21 @@ struct lp855x {
 	struct regulator *enable;	/* regulator for EN/VDDIO input */
 };
 
+#ifdef CONFIG_XIAOMI_CLOVER
+struct lp855x *g_lp = NULL;
+#endif
+
 static int lp855x_write_byte(struct lp855x *lp, u8 reg, u8 data)
 {
 	return i2c_smbus_write_byte_data(lp->client, reg, data);
 }
+
+#ifdef CONFIG_XIAOMI_CLOVER
+static int lp855x_read_byte(struct lp855x *lp, u8 reg)
+{
+	return i2c_smbus_read_byte_data(lp->client, reg);
+}
+#endif
 
 static int lp855x_update_bit(struct lp855x *lp, u8 reg, u8 mask, u8 data)
 {
@@ -198,6 +231,7 @@ static int lp855x_configure(struct lp855x *lp)
 		}
 	}
 
+#ifndef CONFIG_XIAOMI_CLOVER
 	val = pd->initial_brightness;
 	ret = lp855x_write_byte(lp, lp->cfg->reg_brightness, val);
 	if (ret)
@@ -207,6 +241,7 @@ static int lp855x_configure(struct lp855x *lp)
 	ret = lp855x_write_byte(lp, lp->cfg->reg_devicectrl, val);
 	if (ret)
 		goto err;
+#endif
 
 	if (pd->size_program > 0) {
 		for (i = 0; i < pd->size_program; i++) {
@@ -235,6 +270,88 @@ err:
 	return ret;
 }
 
+#ifdef CONFIG_XIAOMI_CLOVER
+static int lp855x_check_id(struct lp855x *lp)
+{
+	int ret = -1;
+	u8 check_times = 5;
+
+	while(check_times--) {
+		ret = lp855x_read_byte(lp, LP8556_ID_REG);
+		if (ret >= 0)
+			return 0;
+		msleep(1);
+	}
+	dev_err(lp->dev, "device check error %d\n", ret);
+	return ret;
+}
+
+static int lp855x_get_config_data(struct lp855x *lp)
+{
+	int ret = 0;
+	switch (lp->chip_id) {
+		case LP8550:
+		case LP8551:
+		case LP8552:
+		case LP8553:
+		case LP8556:
+		lp->cfg = &lp855x_dev_cfg;
+		break;
+		case LP8555:
+		case LP8557:
+		lp->cfg = &lp8557_dev_cfg;
+		break;
+	default:
+		return -EINVAL;
+	}
+	return ret;
+}
+
+int lp855x_init(void)
+{
+	int ret = -1;
+
+	if (!g_lp) {
+		pr_err("lp855x data not found\n");
+		return ret;
+	}
+
+	ret = lp855x_configure(g_lp);
+	if (ret)
+		pr_err("%s: device config err: %d\n", __func__, ret);
+
+	return ret;
+}
+EXPORT_SYMBOL(lp855x_init);
+
+int lp855x_brightness_ctrl(u32 level)
+{
+	int ret = -1;
+	u8 reg;
+
+	if (!g_lp || !g_lp->cfg || !g_lp->client) {
+		pr_err("lp855x data not found\n");
+		return ret;
+	}
+
+	pr_debug("%s: level:%d\n", __func__, level);
+
+	reg = level & 0xFF;
+	ret = lp855x_write_byte(g_lp, LP8556_REG_BRIGHTNESS_LSB, reg);
+	if (ret)
+		pr_err("%s: 0x%02x write err: %d\n", __func__, level, ret);
+	reg = level >> 8;
+	reg = reg & 0x0F;
+	lp855x_write_byte(g_lp, LP8556_REG_BRIGHTNESS_MSB, reg);
+	if (ret)
+		pr_err("%s: 0x%02x write err: %d\n", __func__, level, ret);
+
+	return ret;
+}
+EXPORT_SYMBOL(lp855x_brightness_ctrl);
+#endif
+
+#ifndef CONFIG_XIAOMI_CLOVER
 static void lp855x_pwm_ctrl(struct lp855x *lp, int br, int max_br)
 {
 	unsigned int period = lp->pdata->period_ns;
@@ -309,6 +426,7 @@ static int lp855x_backlight_register(struct lp855x *lp)
 
 	return 0;
 }
+#endif
 
 static ssize_t lp855x_get_chip_id(struct device *dev,
 				struct device_attribute *attr, char *buf)
@@ -332,12 +450,63 @@ static ssize_t lp855x_get_bl_ctl_mode(struct device *dev,
 	return scnprintf(buf, PAGE_SIZE, "%s\n", strmode);
 }
 
+#ifdef CONFIG_XIAOMI_CLOVER
+static ssize_t lp855x_get_bl_reg_dump(struct device *dev,
+				     struct device_attribute *attr, char *buf)
+{
+	struct lp855x *lp = dev_get_drvdata(dev);
+	char *strmode = NULL;
+	int i = 0;
+	int ret = -1;
+	for (i = 0; i <= 0xFF; i++) {
+		ret = lp855x_read_byte(lp, i);
+		pr_err("%s: read reg 0x%02x = 0x%02x\n", __func__, i, ret);
+	}
+
+	if (ret < 0)
+		strmode = "Fail";
+	else
+		strmode = "Pass";
+
+	return scnprintf(buf, PAGE_SIZE, "%s\n", strmode);
+}
+
+static ssize_t lp855x_bl_reg_write(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct lp855x *lp = dev_get_drvdata(dev);
+	int addr,data;
+	int ret;
+
+	if (2 != sscanf(buf, "%x %x", &addr, &data)){
+		pr_err("Not able to get reg data\n");
+		return count;
+	}
+
+	pr_err("%s: write 0x%02x = 0x%02x\n", __func__, addr, data);
+
+	ret = lp855x_write_byte(lp, addr, data);
+	if (ret)
+		pr_err("%s: device write err: %d\n", __func__, ret);
+
+	return count;
+}
+#endif
+
 static DEVICE_ATTR(chip_id, S_IRUGO, lp855x_get_chip_id, NULL);
 static DEVICE_ATTR(bl_ctl_mode, S_IRUGO, lp855x_get_bl_ctl_mode, NULL);
+#ifdef CONFIG_XIAOMI_CLOVER
+static DEVICE_ATTR(bl_reg_dump, S_IRUGO, lp855x_get_bl_reg_dump, NULL);
+static DEVICE_ATTR(bl_reg_write, S_IWUSR|S_IWGRP, NULL, lp855x_bl_reg_write);
+#endif
 
 static struct attribute *lp855x_attributes[] = {
 	&dev_attr_chip_id.attr,
 	&dev_attr_bl_ctl_mode.attr,
+#ifdef CONFIG_XIAOMI_CLOVER
+	&dev_attr_bl_reg_dump.attr,
+	&dev_attr_bl_reg_write.attr,
+#endif
 	NULL,
 };
 
@@ -402,7 +571,11 @@ static int lp855x_parse_dt(struct lp855x *lp)
 static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 {
 	struct lp855x *lp;
+#ifdef CONFIG_XIAOMI_CLOVER
+	int ret = 0;
+#else
 	int ret;
+#endif
 
 	if (!i2c_check_functionality(cl->adapter, I2C_FUNC_SMBUS_I2C_BLOCK))
 		return -EIO;
@@ -411,6 +584,9 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	if (!lp)
 		return -ENOMEM;
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	g_lp = lp;
+#endif
 	lp->client = cl;
 	lp->dev = &cl->dev;
 	lp->chipname = id->name;
@@ -428,12 +604,14 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	else
 		lp->mode = REGISTER_BASED;
 
+#ifndef CONFIG_XIAOMI_CLOVER
 	lp->supply = devm_regulator_get(lp->dev, "power");
 	if (IS_ERR(lp->supply)) {
 		if (PTR_ERR(lp->supply) == -EPROBE_DEFER)
 			return -EPROBE_DEFER;
 		lp->supply = NULL;
 	}
+#endif
 
 	lp->enable = devm_regulator_get_optional(lp->dev, "enable");
 	if (IS_ERR(lp->enable)) {
@@ -448,6 +626,7 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 		}
 	}
 
+#ifndef CONFIG_XIAOMI_CLOVER
 	if (lp->supply) {
 		ret = regulator_enable(lp->supply);
 		if (ret < 0) {
@@ -455,6 +634,7 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 			return ret;
 		}
 	}
+#endif
 
 	if (lp->enable) {
 		ret = regulator_enable(lp->enable);
@@ -472,18 +652,30 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 
 	i2c_set_clientdata(cl, lp);
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	ret = lp855x_check_id(lp);
+	if (ret) {
+		dev_err(lp->dev, "device check id err: %d", ret);
+		return ret;
+	}
+
+	ret = lp855x_get_config_data(lp);
+#else
 	ret = lp855x_configure(lp);
+#endif
 	if (ret) {
 		dev_err(lp->dev, "device config err: %d", ret);
 		return ret;
 	}
 
+#ifndef CONFIG_XIAOMI_CLOVER
 	ret = lp855x_backlight_register(lp);
 	if (ret) {
 		dev_err(lp->dev,
 			"failed to register backlight. err: %d\n", ret);
 		return ret;
 	}
+#endif
 
 	ret = sysfs_create_group(&lp->dev->kobj, &lp855x_attr_group);
 	if (ret) {
@@ -491,7 +683,9 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 		return ret;
 	}
 
+#ifndef CONFIG_XIAOMI_CLOVER
 	backlight_update_status(lp->bl);
+#endif
 	return 0;
 }
 

drivers/video/fbdev/core/fbmem.c
@@ -1063,9 +1063,17 @@ EXPORT_SYMBOL(fb_set_var);
 
 int
 fb_blank(struct fb_info *info, int blank)
-{	
+{
 	struct fb_event event;
 	int ret = -EINVAL, early_ret;
+
+#ifdef CONFIG_XIAOMI_CLOVER
+	if (info->blank == blank) {
+		if (info->fbops->fb_blank)
+			ret = info->fbops->fb_blank(blank, info);
+		return ret;
+	}
+#endif
 
  	if (blank > FB_BLANK_POWERDOWN)
  		blank = FB_BLANK_POWERDOWN;
@@ -1088,6 +1096,11 @@ fb_blank(struct fb_info *info, int blank)
 		if (!early_ret)
 			fb_notifier_call_chain(FB_R_EARLY_EVENT_BLANK, &event);
 	}
+
+#ifdef CONFIG_XIAOMI_CLOVER
+	if (!ret)
+		info->blank = blank;
+#endif
 
  	return ret;
 }
@@ -1682,6 +1695,9 @@ static int do_register_framebuffer(struct fb_info *fb_info)
 		if (!registered_fb[i])
 			break;
 	fb_info->node = i;
+#ifdef CONFIG_XIAOMI_CLOVER
+	fb_info->blank = -1;
+#endif
 	atomic_set(&fb_info->count, 1);
 	mutex_init(&fb_info->lock);
 	mutex_init(&fb_info->mm_lock);

drivers/video/fbdev/msm/mdss_dsi.c
@@ -4537,6 +4537,7 @@ static int mdss_dsi_parse_gpio_params(struct platform_device *ctrl_pdev,
 						__func__, __LINE__);
 	pdata->panel_te_gpio = ctrl_pdata->disp_te_gpio;
 
+#ifndef CONFIG_XIAOMI_CLOVER
 	ctrl_pdata->bklt_en_gpio = of_get_named_gpio(ctrl_pdev->dev.of_node,
 		"qcom,platform-bklight-en-gpio", 0);
 	if (!gpio_is_valid(ctrl_pdata->bklt_en_gpio))
@@ -4545,6 +4546,7 @@ static int mdss_dsi_parse_gpio_params(struct platform_device *ctrl_pdev,
 	ctrl_pdata->bklt_en_gpio_invert =
 			of_property_read_bool(ctrl_pdev->dev.of_node,
 				"qcom,platform-bklight-en-gpio-invert");
+#endif
 
 	ctrl_pdata->avdd_en_gpio = of_get_named_gpio(ctrl_pdev->dev.of_node,
 			"qcom,platform-avdd-en-gpio", 0);

drivers/video/fbdev/msm/mdss_dsi.h
@@ -103,6 +103,9 @@ enum dsi_panel_bl_ctrl {
 	BL_PWM,
 	BL_WLED,
 	BL_DCS_CMD,
+#ifdef CONFIG_XIAOMI_CLOVER
+	BL_I2C_CMD,
+#endif
 	UNKNOWN_CTRL,
 };
 
@@ -499,6 +502,25 @@ struct mdss_dsi_ctrl_pdata {
 	struct mdss_intf_recovery *mdp_callback;
 	struct mdss_intf_ulp_clamp *clamp_handler;
 
+#ifdef CONFIG_XIAOMI_CLOVER
+	struct dsi_panel_cmds CABC_on_cmds;
+	struct dsi_panel_cmds CABC_off_cmds;
+	struct dsi_panel_cmds CE_on_cmds;
+	struct dsi_panel_cmds CE_off_cmds;
+	struct dsi_panel_cmds cold_gamma_cmds;
+	struct dsi_panel_cmds warm_gamma_cmds;
+	struct dsi_panel_cmds default_gamma_cmds;
+	struct dsi_panel_cmds sRGB_on_cmds;
+	struct dsi_panel_cmds sRGB_off_cmds;
+	struct dsi_panel_cmds PM1_cmds;
+	struct dsi_panel_cmds PM2_cmds;
+	struct dsi_panel_cmds PM3_cmds;
+	struct dsi_panel_cmds PM4_cmds;
+	struct dsi_panel_cmds PM5_cmds;
+	struct dsi_panel_cmds PM6_cmds;
+	struct dsi_panel_cmds PM7_cmds;
+	struct dsi_panel_cmds PM8_cmds;
+#endif
 	struct dsi_panel_cmds on_cmds;
 	struct dsi_panel_cmds post_dms_on_cmds;
 	struct dsi_panel_cmds post_panel_on_cmds;
@@ -744,6 +766,11 @@ static inline const char *__mdss_dsi_pm_supply_node_name(
 	default:		return "???";
 	}
 }
+
+#ifdef CONFIG_XIAOMI_CLOVER
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+			struct dsi_panel_cmds *pcmds, u32 flags);
+#endif
 
 static inline u32 mdss_dsi_get_hw_config(struct dsi_shared_data *sdata)
 {

drivers/video/fbdev/msm/mdss_fb.h
@@ -339,6 +339,10 @@ struct msm_fb_data_type {
 	struct task_struct *disp_thread;
 	atomic_t commits_pending;
 	atomic_t kickoff_pending;
+#ifdef CONFIG_XIAOMI_CLOVER
+	atomic_t resume_pending;
+	wait_queue_head_t resume_wait_q;
+#endif
 	wait_queue_head_t commit_wait_q;
 	wait_queue_head_t idle_wait_q;
 	wait_queue_head_t kickoff_wait_q;

sabarop/kernel_xiaomi_sdm660_clover

drivers/video/backlight/lp855x_bl.c
@@ -16,16 +16,38 @@
 #include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/of.h>
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+#include <linux/of_gpio.h>
+#endif
 #include <linux/platform_data/lp855x.h>
 #include <linux/pwm.h>
 #include <linux/regulator/consumer.h>
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+/* LP8556 */
+#define LP8556_DEVICE_ID		0x05
+
+/* LP8556 Registers */
+#define LP8556_BRIGHTNESS_CTRL		0x00
+#define LP8556_DEVICE_CTRL		0x01
+#define LP8556_STATUS_REG		0x02
+#define LP8556_ID_REG			0x03
+#define LP8556_DIRECT_CTRL		0x04
+#define LP8556_REG_BRIGHTNESS_LSB   0x10
+#define LP8556_REG_BRIGHTNESS_MSB   0x11
+#define LP8556_STRING_ENABLE	0x16
+#define LP8556_EPROM_START		0x00
+#define LP8556_EPROM_CFG2		0xA2
+#endif
 
 /* LP8550/1/2/3/6 Registers */
 #define LP855X_BRIGHTNESS_CTRL		0x00
 #define LP855X_DEVICE_CTRL		0x01
 #define LP855X_EEPROM_START		0xA0
 #define LP855X_EEPROM_END		0xA7
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 #define LP8556_EPROM_START		0xA0
+#endif
 #define LP8556_EPROM_END		0xAF
 
 /* LP8555/7 Registers */
@@ -78,10 +100,21 @@ struct lp855x {
 	struct regulator *enable;	/* regulator for EN/VDDIO input */
 };
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+struct lp855x *g_lp = NULL;
+#endif
+
 static int lp855x_write_byte(struct lp855x *lp, u8 reg, u8 data)
 {
 	return i2c_smbus_write_byte_data(lp->client, reg, data);
 }
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+static int lp855x_read_byte(struct lp855x *lp, u8 reg)
+{
+	return i2c_smbus_read_byte_data(lp->client, reg);
+}
+#endif
 
 static int lp855x_update_bit(struct lp855x *lp, u8 reg, u8 mask, u8 data)
 {
@@ -198,6 +231,7 @@ static int lp855x_configure(struct lp855x *lp)
 		}
 	}
 
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 	val = pd->initial_brightness;
 	ret = lp855x_write_byte(lp, lp->cfg->reg_brightness, val);
 	if (ret)
@@ -207,6 +241,7 @@ static int lp855x_configure(struct lp855x *lp)
 	ret = lp855x_write_byte(lp, lp->cfg->reg_devicectrl, val);
 	if (ret)
 		goto err;
+#endif
 
 	if (pd->size_program > 0) {
 		for (i = 0; i < pd->size_program; i++) {
@@ -235,6 +270,88 @@ err:
 	return ret;
 }
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+static int lp855x_check_id(struct lp855x *lp)
+{
+	int ret = -1;
+	u8 check_times = 5;
+
+	while(check_times--) {
+		ret = lp855x_read_byte(lp, LP8556_ID_REG);
+		if (ret >= 0)
+			return 0;
+		msleep(1);
+	}
+	dev_err(lp->dev, "device check error %d\n", ret);
+	return ret;
+}
+
+static int lp855x_get_config_data(struct lp855x *lp)
+{
+	int ret = 0;
+	switch (lp->chip_id) {
+		case LP8550:
+		case LP8551:
+		case LP8552:
+		case LP8553:
+		case LP8556:
+		lp->cfg = &lp855x_dev_cfg;
+		break;
+		case LP8555:
+		case LP8557:
+		lp->cfg = &lp8557_dev_cfg;
+		break;
+	default:
+		return -EINVAL;
+	}
+	return ret;
+}
+
+int lp855x_init(void)
+{
+	int ret = -1;
+
+	if (!g_lp) {
+		pr_err("lp855x data not found\n");
+		return ret;
+	}
+
+	ret = lp855x_configure(g_lp);
+	if (ret)
+		pr_err("%s: device config err: %d\n", __func__, ret);
+
+	return ret;
+}
+EXPORT_SYMBOL(lp855x_init);
+
+int lp855x_brightness_ctrl(u32 level)
+{
+	int ret = -1;
+	u8 reg;
+
+	if (!g_lp || !g_lp->cfg || !g_lp->client) {
+		pr_err("lp855x data not found\n");
+		return ret;
+	}
+
+	pr_debug("%s: level:%d\n", __func__, level);
+
+	reg = level & 0xFF;
+	ret = lp855x_write_byte(g_lp, LP8556_REG_BRIGHTNESS_LSB, reg);
+	if (ret)
+		pr_err("%s: 0x%02x write err: %d\n", __func__, level, ret);
+	reg = level >> 8;
+	reg = reg & 0x0F;
+	lp855x_write_byte(g_lp, LP8556_REG_BRIGHTNESS_MSB, reg);
+	if (ret)
+		pr_err("%s: 0x%02x write err: %d\n", __func__, level, ret);
+
+	return ret;
+}
+EXPORT_SYMBOL(lp855x_brightness_ctrl);
+#endif
+
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 static void lp855x_pwm_ctrl(struct lp855x *lp, int br, int max_br)
 {
 	unsigned int period = lp->pdata->period_ns;
@@ -309,6 +426,7 @@ static int lp855x_backlight_register(struct lp855x *lp)
 
 	return 0;
 }
+#endif
 
 static ssize_t lp855x_get_chip_id(struct device *dev,
 				struct device_attribute *attr, char *buf)
@@ -332,12 +450,63 @@ static ssize_t lp855x_get_bl_ctl_mode(struct device *dev,
 	return scnprintf(buf, PAGE_SIZE, "%s\n", strmode);
 }
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+static ssize_t lp855x_get_bl_reg_dump(struct device *dev,
+				     struct device_attribute *attr, char *buf)
+{
+	struct lp855x *lp = dev_get_drvdata(dev);
+	char *strmode = NULL;
+	int i = 0;
+	int ret = -1;
+	for (i = 0; i <= 0xFF; i++) {
+		ret = lp855x_read_byte(lp, i);
+		pr_err("%s: read reg 0x%02x = 0x%02x\n", __func__, i, ret);
+	}
+
+	if (ret < 0)
+		strmode = "Fail";
+	else
+		strmode = "Pass";
+
+	return scnprintf(buf, PAGE_SIZE, "%s\n", strmode);
+}
+
+static ssize_t lp855x_bl_reg_write(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct lp855x *lp = dev_get_drvdata(dev);
+	int addr,data;
+	int ret;
+
+	if (2 != sscanf(buf, "%x %x", &addr, &data)){
+		pr_err("Not able to get reg data\n");
+		return count;
+	}
+
+	pr_err("%s: write 0x%02x = 0x%02x\n", __func__, addr, data);
+
+	ret = lp855x_write_byte(lp, addr, data);
+	if (ret)
+		pr_err("%s: device write err: %d\n", __func__, ret);
+
+	return count;
+}
+#endif
+
 static DEVICE_ATTR(chip_id, S_IRUGO, lp855x_get_chip_id, NULL);
 static DEVICE_ATTR(bl_ctl_mode, S_IRUGO, lp855x_get_bl_ctl_mode, NULL);
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+static DEVICE_ATTR(bl_reg_dump, S_IRUGO, lp855x_get_bl_reg_dump, NULL);
+static DEVICE_ATTR(bl_reg_write, S_IWUSR|S_IWGRP, NULL, lp855x_bl_reg_write);
+#endif
 
 static struct attribute *lp855x_attributes[] = {
 	&dev_attr_chip_id.attr,
 	&dev_attr_bl_ctl_mode.attr,
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	&dev_attr_bl_reg_dump.attr,
+	&dev_attr_bl_reg_write.attr,
+#endif
 	NULL,
 };
 
@@ -402,7 +571,11 @@ static int lp855x_parse_dt(struct lp855x *lp)
 static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 {
 	struct lp855x *lp;
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	int ret = 0;
+#else
 	int ret;
+#endif
 
 	if (!i2c_check_functionality(cl->adapter, I2C_FUNC_SMBUS_I2C_BLOCK))
 		return -EIO;
@@ -411,6 +584,9 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	if (!lp)
 		return -ENOMEM;
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	g_lp = lp;
+#endif
 	lp->client = cl;
 	lp->dev = &cl->dev;
 	lp->chipname = id->name;
@@ -428,12 +604,14 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	else
 		lp->mode = REGISTER_BASED;
 
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 	lp->supply = devm_regulator_get(lp->dev, "power");
 	if (IS_ERR(lp->supply)) {
 		if (PTR_ERR(lp->supply) == -EPROBE_DEFER)
 			return -EPROBE_DEFER;
 		lp->supply = NULL;
 	}
+#endif
 
 	lp->enable = devm_regulator_get_optional(lp->dev, "enable");
 	if (IS_ERR(lp->enable)) {
@@ -448,6 +626,7 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 		}
 	}
 
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 	if (lp->supply) {
 		ret = regulator_enable(lp->supply);
 		if (ret < 0) {
@@ -455,6 +634,7 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 			return ret;
 		}
 	}
+#endif
 
 	if (lp->enable) {
 		ret = regulator_enable(lp->enable);
@@ -472,18 +652,30 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 
 	i2c_set_clientdata(cl, lp);
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	ret = lp855x_check_id(lp);
+	if (ret) {
+		dev_err(lp->dev, "device check id err: %d", ret);
+		return ret;
+	}
+
+	ret = lp855x_get_config_data(lp);
+#else
 	ret = lp855x_configure(lp);
+#endif
 	if (ret) {
 		dev_err(lp->dev, "device config err: %d", ret);
 		goto disable_vddio;
 	}
 
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 	ret = lp855x_backlight_register(lp);
 	if (ret) {
 		dev_err(lp->dev,
 			"failed to register backlight. err: %d\n", ret);
 		goto disable_vddio;
 	}
+#endif
 
 	ret = sysfs_create_group(&lp->dev->kobj, &lp855x_attr_group);
 	if (ret) {
@@ -491,8 +683,9 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 		goto disable_vddio;
 	}
 
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 	backlight_update_status(lp->bl);
-
+#endif
 	return 0;
 
 disable_vddio:

drivers/video/fbdev/core/fbmem.c
@@ -1061,9 +1061,17 @@ EXPORT_SYMBOL(fb_set_var);
 
 int
 fb_blank(struct fb_info *info, int blank)
-{	
+{
 	struct fb_event event;
 	int ret = -EINVAL, early_ret;
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	if (info->blank == blank) {
+		if (info->fbops->fb_blank)
+			ret = info->fbops->fb_blank(blank, info);
+		return ret;
+	}
+#endif
 
  	if (blank > FB_BLANK_POWERDOWN)
  		blank = FB_BLANK_POWERDOWN;
@@ -1086,6 +1094,11 @@ fb_blank(struct fb_info *info, int blank)
 		if (!early_ret)
 			fb_notifier_call_chain(FB_R_EARLY_EVENT_BLANK, &event);
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	if (!ret)
+		info->blank = blank;
+#endif
 
  	return ret;
 }
@@ -1681,6 +1694,9 @@ static int do_register_framebuffer(struct fb_info *fb_info)
 		if (!registered_fb[i])
 			break;
 	fb_info->node = i;
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	fb_info->blank = -1;
+#endif
 	atomic_set(&fb_info->count, 1);
 	mutex_init(&fb_info->lock);
 	mutex_init(&fb_info->mm_lock);

drivers/video/fbdev/msm/mdss_dsi.c
@@ -4561,6 +4561,7 @@ static int mdss_dsi_parse_gpio_params(struct platform_device *ctrl_pdev,
 						__func__, __LINE__);
 	pdata->panel_te_gpio = ctrl_pdata->disp_te_gpio;
 
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 	ctrl_pdata->bklt_en_gpio = of_get_named_gpio(ctrl_pdev->dev.of_node,
 		"qcom,platform-bklight-en-gpio", 0);
 	if (!gpio_is_valid(ctrl_pdata->bklt_en_gpio))
@@ -4569,6 +4570,7 @@ static int mdss_dsi_parse_gpio_params(struct platform_device *ctrl_pdev,
 	ctrl_pdata->bklt_en_gpio_invert =
 			of_property_read_bool(ctrl_pdev->dev.of_node,
 				"qcom,platform-bklight-en-gpio-invert");
+#endif
 
 	ctrl_pdata->avdd_en_gpio = of_get_named_gpio(ctrl_pdev->dev.of_node,
 			"qcom,platform-avdd-en-gpio", 0);

drivers/video/fbdev/msm/mdss_dsi.h
@@ -97,6 +97,9 @@ enum dsi_panel_bl_ctrl {
 	BL_PWM,
 	BL_WLED,
 	BL_DCS_CMD,
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	BL_I2C_CMD,
+#endif
 	UNKNOWN_CTRL,
 };
 
@@ -493,6 +496,25 @@ struct mdss_dsi_ctrl_pdata {
 	struct mdss_intf_recovery *mdp_callback;
 	struct mdss_intf_ulp_clamp *clamp_handler;
 
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	struct dsi_panel_cmds CABC_on_cmds;
+	struct dsi_panel_cmds CABC_off_cmds;
+	struct dsi_panel_cmds CE_on_cmds;
+	struct dsi_panel_cmds CE_off_cmds;
+	struct dsi_panel_cmds cold_gamma_cmds;
+	struct dsi_panel_cmds warm_gamma_cmds;
+	struct dsi_panel_cmds default_gamma_cmds;
+	struct dsi_panel_cmds sRGB_on_cmds;
+	struct dsi_panel_cmds sRGB_off_cmds;
+	struct dsi_panel_cmds PM1_cmds;
+	struct dsi_panel_cmds PM2_cmds;
+	struct dsi_panel_cmds PM3_cmds;
+	struct dsi_panel_cmds PM4_cmds;
+	struct dsi_panel_cmds PM5_cmds;
+	struct dsi_panel_cmds PM6_cmds;
+	struct dsi_panel_cmds PM7_cmds;
+	struct dsi_panel_cmds PM8_cmds;
+#endif
 	struct dsi_panel_cmds on_cmds;
 	struct dsi_panel_cmds post_dms_on_cmds;
 	struct dsi_panel_cmds post_panel_on_cmds;
@@ -761,6 +783,11 @@ static inline const char *__mdss_dsi_pm_supply_node_name(
 	default:		return "???";
 	}
 }
+
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+			struct dsi_panel_cmds *pcmds, u32 flags);
+#endif
 
 static inline u32 mdss_dsi_get_hw_config(struct dsi_shared_data *sdata)
 {

drivers/video/fbdev/msm/mdss_fb.h
@@ -337,6 +337,10 @@ struct msm_fb_data_type {
 	struct task_struct *disp_thread;
 	atomic_t commits_pending;
 	atomic_t kickoff_pending;
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	atomic_t resume_pending;
+	wait_queue_head_t resume_wait_q;
+#endif
 	wait_queue_head_t commit_wait_q;
 	wait_queue_head_t idle_wait_q;
 	wait_queue_head_t kickoff_wait_q;